2e0bb8fcab2fc7f2bcb0f4e1aa839469c66166fb
r-handsfield <r.handsfield@gmail.com>
2016-02-29 00:09:08 -0500

    Adds grunt packages and tasks to enable server startup and file watch

--- 3-frontEndJsFrameworks/Lectures/week2/conFusion/Gruntfile.js
+++ 3-frontEndJsFrameworks/Lectures/week2/conFusion/Gruntfile.js
@@ -111,6 +111,52 @@ module.exports = function (grunt) {
             options: {
                 assetsdirs: ['dist', 'dist/styles']
             }
+        },
+        
+        // Tasks to start web server, watch files, & reload browser if files are updated
+        watch: {
+            copy: {
+                files: [ 'app/**', '!app/**/*.css', '!app/**/*.js'],
+                tasks: [ 'build' ]
+            },
+            scripts: {
+                files: ['app/scripts/app.js'],
+                tasks:[ 'build']
+            },
+            styles: {
+                files: ['app/styles/mystyles.css'],
+                tasks:['build']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'app/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost', // change to '0.0.0.0' to access this server from outside
+                livereload: 35729
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base: {
+                        path: 'dist',
+                        options: {
+                            index: 'menu.html',
+                            maxAge: 300000
+                        }
+                    }
+                }
+            }
         }
     });
     
@@ -127,6 +173,7 @@ module.exports = function (grunt) {
                             'usemin'
                         ]
                       );
-              
+
+    grunt.registerTask('serve', ['build', 'connect:dist', 'watch']);
     grunt.registerTask('default', ['build']);
 };
